[Done]:1.complete the frustum planes computation, normalization, and visualization; 2. fix the bug in tracker (compute the epipolar line in exhaustive association, it may return None)

Box3dGenerator/frustums.py
@@ -11,8 +11,8 @@ from Box3dGenerator.visualizer import *
 def _calc_boundary_points(depth_img, depth_intrinsic, color2depth_extrinsic, camera2world_extrinsic, bbox2d_dimension,
                           near_thr=0.1, far_thr=5):
     '''
-    calculate the six (right, left, bottom, top, near, far) boundary points
-    of the truncated frustum based on the 2D bbox corner points of color image
+    calculate the six (right, left, bottom, top, near, far) boundary values of the truncated frustum
+     along x, y, z axis based on the 2D bbox corner points of color image
     '''
     xmin, ymin, xmax, ymax = utils._rescale_bbox_dim(bbox2d_dimension)
 
@@ -44,13 +44,11 @@ def _calc_boundary_points(depth_img, depth_intrinsic, color2depth_extrinsic, cam
     b = pts_lb_world[0, 1]
     r = pts_rt_world[0, 0]
     t = pts_rt_world[0, 1]
-    n = min(pts_lb_world[0, 2], pts_rt_world[0, 2])
-    f = max(pts_lb_world[0, 2], pts_rt_world[0, 2])
 
     # check the n and f with the pre-defined thresholds, choose the proper one
-    #TODO: use original z_near and z_far to compute frustums intersection
-    n = min(n, near_thr)
-    f = max(f, far_thr)
+    #TODO: directly use the near and far thresholds
+    n = min(near_thr, pts_lb_world[0, 2], pts_rt_world[0, 2])
+    f = max(far_thr, pts_lb_world[0, 2], pts_rt_world[0, 2])
 
     return r, l, b, t, f, n
 
@@ -76,6 +74,14 @@ def _construct_projection_matrix(r, l, b, t, f, n):
 
     return M
 
+def normalize_plane(p_planes):
+    n = p_planes.shape[0]
+    for i in range(n):
+        mag = np.sqrt(p_planes[i,0]**2 + p_planes[i,1]**2 + p_planes[i,2]**2)
+        p_planes[i, :] /= mag
+
+    return p_planes
+
 
 def _calc_inequalities_coefficients(M):
     '''
@@ -89,40 +95,42 @@ def _calc_inequalities_coefficients(M):
     p_planes = np.zeros((6,4))
 
     # left clipping plane
-    p_planes[0, 0] = M[0, 3] + M[0, 0]
-    p_planes[0, 1] = M[1, 3] + M[1, 0]
-    p_planes[0, 2] = M[2, 3] + M[2, 0]
-    p_planes[0, 3] = M[3, 3] + M[3, 0]
+    p_planes[0, 0] = M[3, 0] + M[0, 0]
+    p_planes[0, 1] = M[3, 1] + M[0, 1]
+    p_planes[0, 2] = M[3, 2] + M[0, 2]
+    p_planes[0, 3] = M[3, 3] + M[0, 3]
 
     # right clipping plane
-    p_planes[1, 0] = M[0, 3] - M[0, 0]
-    p_planes[1, 1] = M[1, 3] - M[1, 0]
-    p_planes[1, 2] = M[2, 3] - M[2, 0]
-    p_planes[1, 3] = M[3, 3] - M[3, 0]
+    p_planes[1, 0] = M[3, 0] - M[0, 0]
+    p_planes[1, 1] = M[3, 1] - M[0, 1]
+    p_planes[1, 2] = M[2, 3] - M[0, 2]
+    p_planes[1, 3] = M[3, 3] - M[0, 3]
 
     # bottom clipping plane
-    p_planes[2, 0] = M[0, 3] + M[0, 1]
-    p_planes[2, 1] = M[1, 3] + M[1, 1]
-    p_planes[2, 2] = M[2, 3] + M[2, 1]
-    p_planes[2, 3] = M[3, 3] + M[3, 1]
+    p_planes[2, 0] = M[3, 0] + M[1, 0]
+    p_planes[2, 1] = M[3, 1] + M[1, 1]
+    p_planes[2, 2] = M[3, 2] + M[1, 2]
+    p_planes[2, 3] = M[3, 3] + M[1, 3]
 
     # top clipping plane
-    p_planes[3, 0] = M[0, 3] - M[0, 1]
-    p_planes[3, 1] = M[1, 3] - M[1, 1]
-    p_planes[3, 2] = M[2, 3] - M[2, 1]
-    p_planes[3, 3] = M[3, 3] - M[3, 1]
+    p_planes[3, 0] = M[3, 0] - M[1, 0]
+    p_planes[3, 1] = M[3, 1] - M[1, 1]
+    p_planes[3, 2] = M[3, 2] - M[1, 2]
+    p_planes[3, 3] = M[3, 3] - M[1, 3]
 
     # near clipping plane
-    p_planes[4, 0] = M[0, 3] + M[0, 2]
-    p_planes[4, 1] = M[1, 3] + M[1, 2]
-    p_planes[4, 2] = M[2, 3] + M[2, 2]
-    p_planes[4, 3] = M[3, 3] + M[3, 2]
+    p_planes[5, 0] = M[3, 0] + M[2, 0]
+    p_planes[5, 1] = M[3, 1] + M[2, 1]
+    p_planes[5, 2] = M[3, 2] + M[2, 2]
+    p_planes[5, 3] = M[3, 3] + M[2, 3]
 
     # far clipping plane
-    p_planes[5, 0] = M[0, 3] - M[0, 2]
-    p_planes[5, 1] = M[1, 3] - M[1, 2]
-    p_planes[5, 2] = M[2, 3] - M[2, 2]
-    p_planes[5, 3] = M[3, 3] - M[3, 2]
+    p_planes[4, 0] = M[3, 0] - M[2, 0]
+    p_planes[4, 1] = M[3, 1] - M[2, 1]
+    p_planes[4, 2] = M[3, 2] - M[2, 2]
+    p_planes[4, 3] = M[3, 3] - M[2, 3]
+
+    p_planes = normalize_plane(p_planes)
 
     return p_planes
 
@@ -169,14 +177,16 @@ def compute_min_max_bounds_in_one_track(scan_dir, objects, trajectory):
         CAM = CAMs[:, :, cfg.SCANNET.CLASS2INDEX[classname]]
 
         frustum_ptcloud = utils.cropped_depth_to_point_cloud_with_cam(depth_img, depth_intrinsic, dimension, CAM)
-        # visualize frustum point cloud
-        visualize_bbox(scan_dir, obj)
-        visualize_frustum_ptcloud_with_cam(frustum_ptcloud)
+        # #visualize frustum point cloud
+        #visualize_bbox(scan_dir, obj)
+        #visualize_frustum_ptcloud_with_cam(frustum_ptcloud)
         frustum_ptclouds.append(frustum_ptcloud)
 
         # generate frustum clipping planes
         frustum_plane = extract_frustum_plane(dimension, depth_img, depth_intrinsic, camera2world_extrinsic,
                                               color2depth_extrinsic)
+        # visualize frustum plan
+        visualize_one_frustum(frustum_plane)
         frustum_planes.append(frustum_plane)
 
     #TODO: use activated frustum ptcloud to filter out invalid object..

Box3dGenerator/visualizer.py
@@ -2,9 +2,11 @@ import os
 import math
 import numpy as np
 import cv2
-# import matplotlib
+import matplotlib
 # matplotlib.use('TkAGG')
 from matplotlib import pyplot as plt
+from matplotlib.pyplot import cm as colormap
+import vtk
 from vtk_visualizer.plot3d import *
 from vtk_visualizer import get_vtk_control
 
@@ -161,7 +163,163 @@ def visualize_frustum_ptcloud_with_cam(frustum_ptcloud):
     '''
     :param frustum_ptcloud: np.ndarray, shape (n,4) [x,y,z,s], s is the class activation score \in (0,1)
     '''
-    activation = frustum_ptcloud[:, 3].reshape(-1, 1)
-    color = np.hstack((activation, activation, activation))
-    color *= 255
+    cm = colormap.get_cmap('inferno')
+    color_mapper = colormap.ScalarMappable(
+        norm=matplotlib.colors.Normalize(vmin=0, vmax=1, clip=True), cmap=cm)
+    color = color_mapper.to_rgba(frustum_ptcloud[:, 3])[:, :3] * 255
     visualize_ptcloud_with_color(frustum_ptcloud[:,0:3], color)
+
+
+def visualize_one_frustum(p_planes):
+
+    colors = vtk.vtkNamedColors()
+
+    planesArray = list(p_planes.flatten())
+    planes = vtk.vtkPlanes()
+    planes.SetFrustumPlanes(planesArray)
+
+    frustumSource = vtk.vtkFrustumSource()
+    frustumSource.ShowLinesOff()
+    frustumSource.SetPlanes(planes)
+
+    shrink = vtk.vtkShrinkPolyData()
+    shrink.SetInputConnection(frustumSource.GetOutputPort())
+    shrink.SetShrinkFactor(.95)
+
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(shrink.GetOutputPort())
+
+    back = vtk.vtkProperty()
+    back.SetColor(colors.GetColor3d("Tomato"))
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().EdgeVisibilityOn()
+    actor.GetProperty().SetColor(colors.GetColor3d("Banana"))
+    actor.SetBackfaceProperty(back)
+
+    # a renderer and render window
+    renderer = vtk.vtkRenderer()
+    renderWindow = vtk.vtkRenderWindow()
+    renderWindow.SetWindowName("Frustum")
+    renderWindow.AddRenderer(renderer)
+
+    # an interactor
+    renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+    renderWindowInteractor.SetRenderWindow(renderWindow)
+
+    # add the actors to the scene
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d("Silver"))
+
+    # Position the camera so that we can see the frustum
+    renderer.GetActiveCamera().SetPosition(1, 0, 0)
+    renderer.GetActiveCamera().SetFocalPoint(0, 0, 0)
+    renderer.GetActiveCamera().SetViewUp(0, 1, 0)
+    renderer.GetActiveCamera().Azimuth(30)
+    renderer.GetActiveCamera().Elevation(30)
+    renderer.ResetCamera()
+
+    # render an image (lights and cameras are created automatically)
+    renderWindow.Render()
+
+    # begin mouse interaction
+    renderWindowInteractor.Start()
+
+    # frustumSource = vtk.vtkFrustumSource()
+    # frustumSource.SetPlanes(planes)
+    # frustumSource.Update()
+    #
+    # frustum = frustumSource.GetOutput()
+    #
+    # mapper = vtk.vtkPolyDataMapper()
+    # if vtk.VTK_MAJOR_VERSION <= 5:
+    #     mapper.SetInput(frustum)
+    # else:
+    #     mapper.SetInputData(frustum)
+    #
+    # actor = vtk.vtkActor()
+    # actor.SetMapper(mapper)
+    #
+    # # a renderer and render window
+    # renderer = vtk.vtkRenderer()
+    # renderWindow = vtk.vtk.vtkRenderWindow()
+    # renderWindow.AddRenderer(renderer)
+    #
+    # # an interactor
+    # renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+    # renderWindowInteractor.SetRenderWindow(renderWindow)
+    #
+    # # add the actors to the scene
+    # renderer.AddActor(actor)
+    # renderer.SetBackground(.2, .1, .3)  # Background color dark purple
+    #
+    # # render an image (lights and cameras are created automatically)
+    # renderWindow.Render()
+    #
+    # # begin mouse interaction
+    # renderWindowInteractor.Start()
+
+
+def test_visualize_frustum():
+    colors = vtk.vtkNamedColors()
+
+    camera = vtk.vtkCamera()
+    camera.SetClippingRange(0.1, 4)
+    planesArray = [0] * 24
+
+    camera.GetFrustumPlanes(0.5, planesArray) # the first argument is the width/height aspect ratio for the viewpoint
+
+    planes = vtk.vtkPlanes()
+    planes.SetFrustumPlanes(planesArray)
+
+    frustumSource = vtk.vtkFrustumSource()
+    frustumSource.ShowLinesOff()
+    frustumSource.SetPlanes(planes)
+
+    shrink = vtk.vtkShrinkPolyData()
+    shrink.SetInputConnection(frustumSource.GetOutputPort())
+    shrink.SetShrinkFactor(1.0)
+
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(shrink.GetOutputPort())
+
+    back = vtk.vtkProperty()
+    back.SetColor(colors.GetColor3d("Tomato"))
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().EdgeVisibilityOn()
+    actor.GetProperty().SetColor(colors.GetColor3d("Banana"))
+    actor.SetBackfaceProperty(back)
+
+    # a renderer and render window
+    renderer = vtk.vtkRenderer()
+    renderWindow = vtk.vtkRenderWindow()
+    renderWindow.SetWindowName("Frustum")
+    renderWindow.AddRenderer(renderer)
+
+    # an interactor
+    renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+    renderWindowInteractor.SetRenderWindow(renderWindow)
+
+    # add the actors to the scene
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d("Silver"))
+
+    # Position the camera so that we can see the frustum
+    renderer.GetActiveCamera().SetPosition(1, 0, 0)
+    renderer.GetActiveCamera().SetFocalPoint(0, 0, 0)
+    renderer.GetActiveCamera().SetViewUp(0, 1, 0)
+    renderer.GetActiveCamera().Azimuth(30)
+    renderer.GetActiveCamera().Elevation(30)
+    renderer.ResetCamera()
+
+    # render an image (lights and cameras are created automatically)
+    renderWindow.Render()
+
+    # begin mouse interaction
+    renderWindowInteractor.Start()
+
+if __name__ == '__main__':
+    test_visualize_frustum()